c18e20ead147651c2554470fc89f969134d745a6
0x0u <m0zurillex@gmail.com>
2020-05-20 19:13:05 +0900

    feat: 認証機能を実装した

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
+import os
 import tweepy
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+
 
 @app.route("/")
 def callback():
-    return render_template("hello.html", greet="Hello, Flask!")
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    redirect_url = auth.get_authorization_url()
+    return render_template("hello.html", redirect_url=redirect_url)
 
 
 if __name__ == "__main__":
