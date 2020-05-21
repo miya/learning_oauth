662b3d61fca34c0edb390bc90fc548d25823918c
0x0u <m0zurillex@gmail.com>
2020-05-22 01:49:25 +0900

    refactor: 認証用のURLを関数として独立させた

--- src/app.py
+++ src/app.py
@@ -10,13 +10,7 @@ CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 CALLBACK_URL = os.environ.get("CALLBACK_URL")
 
 
-@app.route("/")
-def root():
-    return render_template("hello.html")
-
-
-# 認証用のリダイレクトURLを生成、リダイレクトさせる
-@app.route("/", methods=["POST"])
+# 認証用URLを生成する
 def twitter_auth():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL)
     try:
@@ -24,6 +18,31 @@ def twitter_auth():
         return redirect(redirect_url)
     except tweepy.TweepError:
         return render_template("error.html")
+
+
+# ユーザー情報を取得する
+def get_user_data(api):
+    user = api.me()
+    user_data_dic = {}
+
+    user_data_dic.update({
+        "name": user.name,
+        "screen_name": user.screen_name,
+        "id_str": user.id_str,
+        "location": user.location,
+        "description": user.description
+    })
+    return user_data_dic
+
+
+@app.route("/")
+def root():
+    return render_template("hello.html")
+
+
+@app.route("/", methods=["POST"])
+def login():
+    return twitter_auth()
 
 
 @app.route("/callback")
@@ -49,20 +68,6 @@ def callback():
 
     return render_template("result.html", user=user)
 
-
-def get_user_data(api):
-    user = api.me()
-    user_data_dic = {}
-
-    user_data_dic.update({
-        "name": user.name,
-        "screen_name": user.screen_name,
-        "id_str": user.id_str,
-        "location": user.location,
-        "description": user.description
-    })
-
-    return user_data_dic
 
 
 if __name__ == "__main__":
