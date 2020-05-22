c64d098fdff20464a9d34db559b7e91206cdee5e
0x0u <m0zurillex@gmail.com>
2020-05-22 22:06:55 +0900

    feat: リザルト画面でリロードしたら認証画面にリダイレクトされるようにした

--- src/app.py
+++ src/app.py
@@ -3,16 +3,16 @@ import tweepy
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
-app.secret_key = "hogehoge"
 
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
-CALLBACK_URL = os.environ.get("CALLBACK_URL")
+# CALLBACK_URL = os.environ.get("CALLBACK_URL")
+CALLBACK_URL = "https://8b77102f.ngrok.io/callback"
 
 
 # 認証用URLを生成する
 def twitter_auth():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     try:
         redirect_url = auth.get_authorization_url()
         return redirect(redirect_url)
@@ -22,52 +22,52 @@ def twitter_auth():
 
 # ユーザー情報を取得する
 def get_user_data(api):
-    user = api.me()
+    user = api.me()._json
     user_data_dic = {
-        "id": user.id,
-        "id_str": user.id_str,
-        "name": user.name,
-        "screen_name": user.screen_name,
-        "location": user.location,
-        "profile_location": user.profile_location,
-        "description": user.description,
-        "url": user.url,
-        "protected": user.protected,
-        "followers_count": user.followers_count,
-        "friends_count": user.friends_count,
-        "listed_count": user.listed_count,
-        "created_at": user.created_at,
-        "favourites_count": user.favourites_count,
-        "utc_offset": user.utc_offset,
-        "time_zone": user.time_zone,
-        "geo_enabled": user.geo_enabled,
-        "verified": user.verified,
-        "statuses_count": user.statuses_count,
-        "lang": user.lang,
-        "contributors_enabled": user.contributors_enabled,
-        "is_translator": user.is_translator,
-        "is_translation_enabled": user.is_translation_enabled,
-        "profile_background_color": user.profile_background_color,
-        "profile_background_image_url": user.profile_background_image_url,
-        "profile_background_image_url_https": user.profile_background_image_url_https,
-        "profile_background_tile": user.profile_background_tile,
-        "profile_image_url": user.profile_image_url,
-        "profile_image_url_https": user.profile_image_url_https,
-        "profile_banner_url": user.profile_banner_url,
-        "profile_link_color": user.profile_link_color,
-        "profile_sidebar_border_color": user.profile_sidebar_border_color,
-        "profile_sidebar_fill_color": user.profile_sidebar_fill_color,
-        "profile_text_color": user.profile_text_color,
-        "profile_use_background_image": user.profile_use_background_image,
-        "has_extended_profile": user.has_extended_profile,
-        "default_profile": user.default_profile,
-        "default_profile_image": user.default_profile_image,
-        "following": user.following,
-        "follow_request_sent": user.follow_request_sent,
-        "notifications": user.notifications,
-        "translator_type": user.translator_type,
-        "suspended": user.suspended,
-        "needs_phone_verification": user.needs_phone_verification,
+        "id": user.get("id"),
+        "id_str": user.get("id_str"),
+        "name": user.get("name"),
+        "screen_name": user.get("screen_name"),
+        "location": user.get("location"),
+        "profile_location": user.get("profile_location"),
+        "description": user.get("description"),
+        "url": user.get("url"),
+        "protected": user.get("protected"),
+        "followers_count": user.get("followers_count"),
+        "friends_count": user.get("friends_count"),
+        "listed_count": user.get("listed_count"),
+        "created_at": user.get("created_at"),
+        "favourites_count": user.get("favourites_count"),
+        "utc_offset": user.get("utc_offset"),
+        "time_zone": user.get("time_zone"),
+        "geo_enabled": user.get("geo_enabled"),
+        "verified": user.get("verified"),
+        "statuses_count": user.get("statuses_count"),
+        "lang": user.get("lang"),
+        "contributors_enabled": user.get("contributors_enabled"),
+        "is_translator": user.get("is_translator"),
+        "is_translation_enabled": user.get("is_translation_enabled"),
+        "profile_background_color": user.get("profile_background_color"),
+        "profile_background_image_url": user.get("profile_background_image_url"),
+        "profile_background_image_url_https": user.get("profile_background_image_url_https"),
+        "profile_background_tile": user.get("profile_background_tile"),
+        "profile_image_url": user.get("profile_image_url"),
+        "profile_image_url_https": user.get("profile_image_url_https"),
+        "profile_banner_url": user.get("profile_banner_url"),
+        "profile_link_color": user.get("profile_link_color"),
+        "profile_sidebar_border_color": user.get("profile_sidebar_border_color"),
+        "profile_sidebar_fill_color": user.get("profile_sidebar_fill_color"),
+        "profile_text_color": user.get("profile_text_color"),
+        "profile_use_background_image": user.get("profile_use_background_image"),
+        "has_extended_profile": user.get("has_extended_profile"),
+        "default_profile": user.get("default_profile"),
+        "default_profile_image": user.get("default_profile_image"),
+        "following": user.get("following"),
+        "follow_request_sent": user.get("follow_request_sent"),
+        "notifications": user.get("notifications"),
+        "translator_type": user.get("translator_type"),
+        "suspended": user.get("suspended"),
+        "needs_phone_verification": user.get("needs_phone_verification"),
     }
     return user_data_dic
 
@@ -97,7 +97,11 @@ def callback():
         "oauth_token_secret": verifier
     }
 
-    auth.get_access_token(verifier)
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        return twitter_auth()
+
     auth.set_access_token(auth.access_token, auth.access_token_secret)
 
     api = tweepy.API(auth)
